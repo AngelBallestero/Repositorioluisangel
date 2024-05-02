857d95bae1310a7e4a40269f73cfd5eada624d76
Luis Angel <luisgilballestero@gmail.com>
2024-05-02 12:09:24 -0500

    h

--- index.php
+++ index.php
@@ -4,26 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LISTA DE INICIO DE SESION</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Enlaza tu archivo CSS -->
 </head>
 <body>
-<center>
-    <h1>INICIO</h1>
-    
-    <h2>INICIO SESION</h2>
-    <form action="Iniciar_Session.php" method="POST">
-        <input type="text" name="username" placeholder="username" required>
-        <br/>
-        <input type="password" name="passwords" placeholder="passwords" required>
-        <br/>
-        <input type="submit" name="btnIniciar" value="iniciar sesion"></input>
-
-    </form>
     <center>
-<html>
-
+        <h1 class="titulo">INICIO</h1>
+        <h2 class="titulo">INICIO SESION</h2>
+        <form action="Iniciar_Session.php" method="POST">
+            <input type="text" name="username" placeholder="username" required>
+            <br/>
+            <input type="password" name="passwords" placeholder="passwords" required>
+            <br/>
+            <input type="submit" name="btnIniciar" value="iniciar sesion"></input>
+        </form>
+    </center>
+</body>
 </html>
 
-<?
 
 
    
